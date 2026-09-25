2766d5f18134cdcacb6d6b0c16a9c432248b0e8e
Irwin1138 <irwin1139@gmail.com>
2026-09-25 09:34:10 +0300

    Revert "Enable LADSPA support in ffmpeg configuration"
    This reverts commit fece19504845321cecbb9c127287cea6c6e7f682.

--- packages/ffmpeg.cmake
+++ packages/ffmpeg.cmake
@@ -61,7 +61,6 @@ ExternalProject_Add(ffmpeg
         --enable-version3
         --enable-avisynth
         --enable-vapoursynth
-        --enable-ladspa
         --enable-libass
         --enable-libbluray
         --enable-libdvdnav
